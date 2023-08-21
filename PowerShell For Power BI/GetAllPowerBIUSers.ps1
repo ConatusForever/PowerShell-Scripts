5cd1a1d3978b013e49bbdd21f7f12d09d34e3650
Hakeem Lawrence <84429597+ConatusForever@users.noreply.github.com>
2023-08-21 18:58:51 -0400

    Add files via upload

---
+++ PowerShell For Power BI/GetAllPowerBIUSers.ps1
@@ -0,0 +1,15 @@
+#PowerShell Automation | Get ALl Power BI Users | Export as CSV to Folder
+
+connect-PowerBIServiceAccount 
+
+$Workspaces = Get-PowerBIWorkspace -Scope Organization -All
+$Workspaces |
+    foreach-object {
+        $Workspace = $_.name
+        foreach ($User in $_.Users) {
+            [PSCustomObject]@{
+                workspace = $Workspace
+                User = $User.UserPrincipalName
+            }
+        }
+    } | Export-CSV -Path "C:\Users\hakeem.lawrence\OneDrive\Business Intelligence\PowerBIGroupMembers.csv" -NoTypeInformation
